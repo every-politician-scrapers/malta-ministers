4ab1f236cfcbde8598b5fafe68d81b309ab33a66
Tony Bowden <tony@tmtm.com>
2021-11-23 20:48:10 +0200

    Scrape multi-page site

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -4,23 +4,46 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-class MemberList
-  class Member
-    def name
-      noko.css('.name').text.tidy
-    end
-
-    def position
-      noko.css('.position').text.tidy
-    end
-  end
-
-  class Members
-    def member_container
-      noko.css('.member')
-    end
+class String
+  def ztidy
+    gsub(/[\u200B-\u200D\uFEFF]/, '').tidy
   end
 end
 
-file = Pathname.new 'html/official.html'
-puts EveryPoliticianScraper::FileData.new(file).csv
+class Member < Scraped::HTML
+  field :name do
+    fields.map(&:text).map(&:ztidy)
+          .select { |txt| txt.include? ' MP' }.first
+          .gsub(/(Prime )?Minister: /, '')
+  end
+
+  field :position do
+    fields.map(&:text).map(&:ztidy)
+          .select { |txt| txt.include? 'MINIST' }.first
+          .gsub('MINISTRY', 'MINISTER')
+          .gsub(/^OFFICE OF THE PRIME MINISTER/, 'Prime Minister')
+          .gsub(/^OFFICE OF THE DEPUTY PRIME MINISTER/, 'Deputy Prime Minister')
+          .split(/ AND (?=MINISTER)/)
+  end
+
+  field :url do
+    noko.xpath('//link[@rel="canonical"]/@href').text
+  end
+
+  private
+
+  def fields
+    noko.css('.ms-rtestate-field strong,span')
+  end
+end
+
+dir = Pathname.new 'mirror'
+data = dir.children.reject { |file| file.to_s.include? 'default.aspx' }.sort.flat_map do |file|
+  request = Scraped::Request.new(url: file, strategies: [LocalFileRequest])
+  data = Member.new(response: request.response).to_h
+  [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
+end.uniq
+
+ORDER = %i[name position url].freeze
+puts ORDER.to_csv
+data.each { |row| puts row.values_at(*ORDER).to_csv }
